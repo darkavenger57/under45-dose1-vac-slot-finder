3f24c2332e36a6df09fb7e7850eb3ee7dadaf219
darkavenger57 <darkavenger57@yahoo.co.in>
2021-06-12 19:28:13 +0530

    Create DBtable.sql

---
+++ DBtable.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS public.calendar_availability
+(
+    center_id integer NOT NULL,
+    vaccine character varying(100) COLLATE pg_catalog."default",
+    available_capacity_dose1 integer,
+    available_capacity_dose2 integer,
+    slots character varying(2000) COLLATE pg_catalog."default",
+    min_age_limit integer NOT NULL,
+    available_date character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    name character varying(2000) COLLATE pg_catalog."default",
+    pincode integer,
+    fee_type character varying(20) COLLATE pg_catalog."default",
+    detected_date_time timestamp without time zone
+)
